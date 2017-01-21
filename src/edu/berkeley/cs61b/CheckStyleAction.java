080393bd9362bab52b127870628bb9c5acaf80bb
aegamesi <admin.aegamesi@gmail.com>
2017-01-20 21:06:29 -0800

    Add style checker output tool window

--- src/edu/berkeley/cs61b/CheckStyleAction.java
+++ src/edu/berkeley/cs61b/CheckStyleAction.java
@@ -1,12 +1,17 @@
 package edu.berkeley.cs61b;
 
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
 
 public class CheckStyleAction extends AnAction {
 
@@ -14,8 +19,18 @@ public class CheckStyleAction extends AnAction {
 	public void actionPerformed(AnActionEvent event) {
 		Project project = event.getData(PlatformDataKeys.PROJECT);
 		VirtualFile file = event.getData(CommonDataKeys.VIRTUAL_FILE);
-		if (file != null) {
-			Messages.showMessageDialog(project, file.getPath(), "File Info", Messages.getInformationIcon());
+
+		if (project != null && file != null) {
+			// get the Style Checker ToolWindow
+			final ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow("Style Checker");
+			toolWindow.activate(() -> {
+				// upon activation, print some stuff out.
+				ContentManager contentManager = toolWindow.getContentManager();
+				Content content = contentManager.findContent("");
+				ConsoleView consoleView = (ConsoleView) content.getComponent();
+
+				consoleView.print(file.getPath(), ConsoleViewContentType.ERROR_OUTPUT);
+			});
 		}
 	}
 }
